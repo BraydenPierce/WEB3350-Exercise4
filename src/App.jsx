2aa790b62b82f85c3a3ff167676fd52ded38a2d3
Brayden Pierce <braydenpierce@mail.weber.edu>
2025-02-16 12:30:47 -0700

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div></div>
+      <h1>Weather</h1>
+      <div className="card"></div>
+    </>
+  );
+}
+
+export default App;
