6177ef4a3222bcc34331f2710a134743581723f8
Brayden Pierce <braydenpierce@mail.weber.edu>
2025-02-17 17:51:24 -0700

    Trying to get information from API key

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
-import { useState } from "react";
+import axios from "axios";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [weathers, setWeather] = useState([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(
+        "http://api.openweathermap.org/data/2.5/weather?q=ogden&appid=a6573011df60d44d409ba0ca008039aa"
+      )
+      .then((res) => {
+        setWeather(res.data.weather);
+      })
+      .catch((err) => {
+        setError(err.message);
+      });
+  }, []);
+
   return (
-    <>
-      <div></div>
-      <h1>Weather</h1>
-      <div className="card"></div>
-    </>
+    <div className="container">
+      {error && <p>{error}</p>}
+      <div className="weather-grid">
+        {weathers.map((weather) => (
+          <div className="weather-info">
+            <p>Forecast:{weather.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
